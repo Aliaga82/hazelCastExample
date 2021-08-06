63ad9f6950b4256c80f7a87cef04359e908f13ee
Aliagha <aghayevaliagha@gmail.com>
2021-08-07 02:17:45 +0400

    th commit

--- src/main/java/com/example/hazelcastExample/service/BookService.java
+++ src/main/java/com/example/hazelcastExample/service/BookService.java
@@ -3,12 +3,13 @@ package com.example.hazelcastExample.service;
 import com.example.hazelcastExample.domain.Book;
 import com.example.hazelcastExample.repository.BookRepository;
 import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
+@Log4j2
 
 public class BookService {
     private final BookRepository bookRepository;
@@ -19,6 +20,7 @@ public class BookService {
     }
     public Book findBookInslowSource(String isbn){
         try {
+            log.trace("from data atabase");
            Thread.sleep(3000);
         } catch (InterruptedException e){
           e.printStackTrace();
